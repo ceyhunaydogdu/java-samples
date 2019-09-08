509b4ed5cf9478fb2b8f09d848cb013baec8b9e9
ceyhunaydogdu <aydogdu.ceyhun@gmail.com>
2019-09-08 08:29:30 +0300

    updated imports

--- java-jshell/src/main/java/com/ca/samples/jshell/JShellApp.java
+++ java-jshell/src/main/java/com/ca/samples/jshell/JShellApp.java
@@ -9,57 +9,60 @@ import java.util.stream.Collectors;
 
 import jdk.jshell.*;
 import jdk.jshell.Snippet.Status;
+import jdk.jshell.tool.JavaShellToolBuilder;
 
 class JShellApp {
 
-    public static void main(String[] args) {
-        Console console = System.console();
-        try (JShell js = JShell.create()) {
-            loadDefaultPackages(js);
-            do {
-                System.out.print("Enter some Java code: ");
-                String input = console.readLine();
-                if (input == null) {
-                    break;
-                }
-                List<SnippetEvent> events = analyzeCode(js,input)   
-                                                .stream().map(js::eval).flatMap(List::stream)
-                                                .collect(Collectors.toList());
-                for (SnippetEvent e : events) {
-                    StringBuilder sb = new StringBuilder();
-                    if (e.causeSnippet() == null) {
-                        //  We have a snippet creation event
-                        switch (e.status()) {
-                            case VALID:
-                                sb.append("Successful ");
-                                break;
-                            case RECOVERABLE_DEFINED:
-                                sb.append("With unresolved references ");
-                                break;
-                            case RECOVERABLE_NOT_DEFINED:
-                                sb.append("Possibly reparable, failed  ");
-                                break;
-                            case REJECTED:
-                                sb.append("Failed ");
-                                break;
-                        }
-                        if (e.previousStatus() == Status.NONEXISTENT) {
-                            sb.append("addition");
-                        } else {
-                            sb.append("modification");
-                        }
-                        sb.append(" of ");
-                        sb.append(e.snippet().source());
-                        System.out.println(sb);
-                        if (e.value() != null) {
-                            System.out.printf("Value is: %s\n", e.value());
-                        }
-                        System.out.flush();
-                    }
-                }
-            } while (true);
-        }
-        System.out.println("\nGoodbye");
+    public static void main(String[] args) throws Exception {
+
+        JavaShellToolBuilder.builder().run();
+        // Console console = System.console();
+        // try (JShell js = JShell.create()) {
+        //     loadDefaultPackages(js);
+        //     do {
+        //         System.out.print("Enter some Java code: ");
+        //         String input = console.readLine();
+        //         if (input == null) {
+        //             break;
+        //         }
+        //         List<SnippetEvent> events = analyzeCode(js,input)   
+        //                                         .stream().map(js::eval).flatMap(List::stream)
+        //                                         .collect(Collectors.toList());
+        //         for (SnippetEvent e : events) {
+        //             StringBuilder sb = new StringBuilder();
+        //             if (e.causeSnippet() == null) {
+        //                 //  We have a snippet creation event
+        //                 switch (e.status()) {
+        //                     case VALID:
+        //                         sb.append("Successful ");
+        //                         break;
+        //                     case RECOVERABLE_DEFINED:
+        //                         sb.append("With unresolved references ");
+        //                         break;
+        //                     case RECOVERABLE_NOT_DEFINED:
+        //                         sb.append("Possibly reparable, failed  ");
+        //                         break;
+        //                     case REJECTED:
+        //                         sb.append("Failed ");
+        //                         break;
+        //                 }
+        //                 if (e.previousStatus() == Status.NONEXISTENT) {
+        //                     sb.append("addition");
+        //                 } else {
+        //                     sb.append("modification");
+        //                 }
+        //                 sb.append(" of ");
+        //                 sb.append(e.snippet().source());
+        //                 System.out.println(sb);
+        //                 if (e.value() != null) {
+        //                     System.out.printf("Value is: %s\n", e.value());
+        //                 }
+        //                 System.out.flush();
+        //             }
+        //         }
+        //     } while (true);
+        // }
+        // System.out.println("\nGoodbye");
     }
 
     public static List<String> analyzeCode(JShell js, String code) {
@@ -74,11 +77,12 @@ class JShellApp {
     }
 
     public static void loadDefaultPackages(JShell js) {
-        String imports="import java.io.*; import java.math.*; import java.net.*; import java.nio.file.*; import java.util.*; import java.util.concurrent.*; import java.util.funtion.*; import java.util.prefs.*; import java.util.regex.*; import java.util.stream.*;";
         System.out.println("---> START : loading default packages");
-        analyzeCode(js,imports).stream().map(js::eval).flatMap(List::stream).forEach(e->System.out.println(e.value()));   
-        js.eval("import java.util.*;").stream().forEach(e->System.out.println(e.value()));
-        js.eval("import java.util.stream.*;").stream().forEach(e->System.out.println(e.value()));
+        // String imports="import java.io.*; \n import java.math.*; \n import java.net.*; \n import java.nio.file.*; \n import java.util.*; \n import java.util.concurrent.*; import java.util.funtion.*; import java.util.prefs.*; import java.util.regex.*; import java.util.stream.*;";
+        // String imports="import java.io.*; \n import java.math.*; \n import java.net.*; \n import java.nio.file.*; \n import java.util.*; \n import java.util.concurrent.*; \n import java.util.funtion.*; \n import java.util.prefs.*; \n import java.util.regex.*; \n import java.util.stream.*;";
+        // analyzeCode(js,imports).stream().map(js::eval).flatMap(List::stream).forEach(e->System.out.println(e.value()));   
+        // js.eval("import java.util.*;").stream().forEach(e->System.out.println(e.value()));
+        js.eval("import java.util.stream.*;");
         js.imports().findAny().ifPresentOrElse(is-> {
                 System.out.println(is.source()); 
             }, ()->System.out.println("No package imported"));
